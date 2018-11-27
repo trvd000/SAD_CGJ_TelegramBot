fe989c92b3076f740541287202e7b046fc18aaa4
LUCAS EUGENIO SABIO SILVERIO <trvd000@gmail.com>
2018-11-27 12:47:49 -0400

    primeiro upload

---
+++ database/database_setup.py
@@ -0,0 +1,26 @@
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class Servidores(Base):
+    __tablename__ = 'servidores'
+    nome = Column(String(80), nullable=False)
+    telegram_id = Column(String(50), nullable=False)
+    matricula = Column(Integer(15), nullable=False)
+    cargo = Column(String(20), nullable=False)
+
+    @property
+    def serialize(self):
+        return{
+            'nome' = self.nome
+            'id' = self.telegram_id
+            'matricula' = self.matricula
+            'cargo' = self.cargo
+        }
+
+engine = create_engine('sqlite:///servidores.db')
+Base.metadata.create_all(engine)
